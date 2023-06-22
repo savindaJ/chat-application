477c22f047b8a1ed2a4191987dcac0ec7ebef172
SavindaJayasekara <thantrige32@gmail.com>
2023-06-22 18:15:11 +0530

    fix chat

--- src/main/java/lk/ijse/chatApplication/controller/ClientController.java
+++ src/main/java/lk/ijse/chatApplication/controller/ClientController.java
@@ -1,4 +1,29 @@
 package lk.ijse.chatApplication.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class ClientController {
+    public Label lblClientName;
+    public TextField txtMessage;
+
+    public void mouseEnterAnim(MouseEvent event) {
+    }
+
+    public void mouseExitAnim(MouseEvent event) {
+    }
+
+    public void mouseClickOnAction(MouseEvent event) {
+    }
+
+    public void txtMessageOnAction(ActionEvent event) {
+    }
+
+    public void btnimojiOnAction(MouseEvent event) {
+    }
+
+    public void btnfileOnAction(MouseEvent event) {
+    }
 }
