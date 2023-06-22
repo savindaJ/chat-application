527b71bb77065752e6d95841e6bba086460441fa
SavindaJayasekara <thantrige32@gmail.com>
2023-06-22 18:23:20 +0530

    added anim

--- src/main/java/lk/ijse/chatApplication/controller/ClientController.java
+++ src/main/java/lk/ijse/chatApplication/controller/ClientController.java
@@ -1,10 +1,15 @@
 package lk.ijse.chatApplication.controller;
 
+import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public class ClientController {
     public Label lblClientName;
@@ -16,9 +21,33 @@ public class ClientController {
     }
 
     public void mouseEnterAnim(MouseEvent event) {
+        if (event.getSource() instanceof javafx.scene.image.ImageView) {
+            javafx.scene.image.ImageView icon = (ImageView) event.getSource();
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+//            glow.setColor(Color.valueOf("#EF233C"));
+            glow.setColor(Color.CORNFLOWERBLUE);
+            glow.setWidth(15);
+            glow.setHeight(15);
+            glow.setRadius(15);
+            icon.setEffect(glow);
+        }
     }
 
     public void mouseExitAnim(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+            icon.setEffect(null);
+        }
     }
 
     public void mouseClickOnAction(MouseEvent event) {
