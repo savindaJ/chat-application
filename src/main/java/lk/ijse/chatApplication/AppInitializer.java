3f8b053caed5a8e2343513e5bb8b59ca53897640
SavindaJayasekara <thantrige32@gmail.com>
2023-06-22 18:01:23 +0530

    first commit

---
+++ src/main/java/lk/ijse/chatApplication/AppInitializer.java
@@ -0,0 +1,15 @@
+package lk.ijse.chatApplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/lk/ijse/chatApplication/Launcher.java
@@ -0,0 +1,7 @@
+package lk.ijse.chatApplication;
+
+public class Launcher {
+    public static void main(String[] args) {
+        AppInitializer.launch(args);
+    }
+}
