9d9d1dcd6e30b5502e67851592aa220eedef5180
SavindaJayasekara <thantrige32@gmail.com>
2023-06-22 18:18:34 +0530

    fix home controller

--- src/main/java/lk/ijse/chatApplication/controller/HomeFormController.java
+++ src/main/java/lk/ijse/chatApplication/controller/HomeFormController.java
@@ -1,25 +1,79 @@
 package lk.ijse.chatApplication.controller;
 
+import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class HomeFormController {
     public TextField txtjon;
     public ImageView btnjon;
 
     public void txtjonOnActon(ActionEvent event) {
+        btnjonClick();
     }
 
-    public void btnjonClick(MouseEvent event) {
+    public void btnjonClick() {
+        if (txtjon.getText().equals("") || txtjon.getText().equals("please enter your name !")){
+            txtjon.setStyle("-fx-border-color: red");
+            txtjon.setText("please enter your name !");
+            txtjon.selectAll();
+            return;
+        }
+//        Detail.name=txtjon.getText();
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/client_form.fxml"));
+        stage.getIcons().add(new Image("/assets/send3D.png"));
+        try {
+            stage.setScene(new Scene(fxmlLoader.load()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        stage.setResizable(false);
+//        stage.setTitle(Detail.name+" in your chat");
+        stage.show();
+        stage.centerOnScreen();
+        txtjon.clear();
     }
 
     public void mouseEnterAnim(MouseEvent event) {
+        if (event.getSource() instanceof javafx.scene.image.ImageView) {
+            javafx.scene.image.ImageView icon = (ImageView) event.getSource();
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+//            glow.setColor(Color.valueOf("#EF233C"));
+            glow.setColor(Color.CORNFLOWERBLUE);
+            glow.setWidth(15);
+            glow.setHeight(15);
+            glow.setRadius(15);
+            icon.setEffect(glow);
+        }
     }
 
     public void mouseExitAnim(MouseEvent event) {
-
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+            icon.setEffect(null);
+        }
     }
 }
