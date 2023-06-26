a1a36c8cbfc6a68f5c8e74eb78d483e5a8b74f06
SavindaJayasekara <thantrige32@gmail.com>
2023-06-26 10:51:16 +0530

    added components

--- src/main/java/lk/ijse/chatApplication/controller/ClientController.java
+++ src/main/java/lk/ijse/chatApplication/controller/ClientController.java
@@ -57,19 +57,19 @@ public class ClientController {
     private String clientName;
     private PrintWriter writer;
 
-    byte[] emojiByteCode = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x8C};
-    byte[] emojiByteCode1 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x8D};
-    byte[] emojiByteCode2 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x8F};
-    byte[] emojiByteCode3 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x93};
-    byte[] emojiByteCode4 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x98};
-    byte[] emojiByteCode5 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x82};
+    private final byte[] emojiByteCode = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x8C};
+    private final byte[] emojiByteCode1 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x8D};
+    private final byte[] emojiByteCode2 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x8F};
+    private final byte[] emojiByteCode3 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x93};
+    private final byte[] emojiByteCode4 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x98};
+    private final byte[] emojiByteCode5 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x82};
 
-    String img1 = new String(emojiByteCode, Charset.forName("UTF-8"));
-    String img2 = new String(emojiByteCode1, Charset.forName("UTF-8"));
-    String img3 = new String(emojiByteCode2, Charset.forName("UTF-8"));
-    String img4 = new String(emojiByteCode3, Charset.forName("UTF-8"));
-    String img5 = new String(emojiByteCode4, Charset.forName("UTF-8"));
-    String img6 = new String(emojiByteCode5, Charset.forName("UTF-8"));
+    private final String img1 = new String(emojiByteCode, Charset.forName("UTF-8"));
+    private final String img2 = new String(emojiByteCode1, Charset.forName("UTF-8"));
+    private final String img3 = new String(emojiByteCode2, Charset.forName("UTF-8"));
+    private final String img4 = new String(emojiByteCode3, Charset.forName("UTF-8"));
+    private final String img5 = new String(emojiByteCode4, Charset.forName("UTF-8"));
+    private final String img6 = new String(emojiByteCode5, Charset.forName("UTF-8"));
 
     @FXML
     void initialize(){
